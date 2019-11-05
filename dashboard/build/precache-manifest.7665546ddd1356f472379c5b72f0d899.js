9971e1578df3e6d0d9eeff48b61c7e5ca3cd6b2d
czuniga <chad.zuniga10@gmail.com>
2019-11-05 11:39:16 -0700

    made build visible to github for deployment

---
+++ dashboard/build/precache-manifest.7665546ddd1356f472379c5b72f0d899.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179a75d56e44633f24672357b87b15ea",
+    "url": "/LeaseDeployDashboard/index.html"
+  },
+  {
+    "revision": "77beafa51a2126df9d5f",
+    "url": "/LeaseDeployDashboard/static/css/main.d39edc37.chunk.css"
+  },
+  {
+    "revision": "d49be7f080c4b5e882fc",
+    "url": "/LeaseDeployDashboard/static/js/2.7f30c843.chunk.js"
+  },
+  {
+    "revision": "77beafa51a2126df9d5f",
+    "url": "/LeaseDeployDashboard/static/js/main.b5b01704.chunk.js"
+  },
+  {
+    "revision": "4ceab00013cbb655d293",
+    "url": "/LeaseDeployDashboard/static/js/runtime-main.bc57fa66.js"
+  }
+]);
